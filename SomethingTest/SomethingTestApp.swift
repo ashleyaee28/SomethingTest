5f8ee4e747ef896f545a7c0a0b91cbcc6532e45d
Ashley Barrera <ashleyaee83@gmail.com>
2023-07-18 09:31:02 -0500

    Initial Commit

---
+++ SomethingTest/SomethingTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SomethingTestApp.swift
+//  SomethingTest
+//
+//  Created by Scholar on 7/18/23.
+//
+
+import SwiftUI
+
+@main
+struct SomethingTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
